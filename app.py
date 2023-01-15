706d77ede9842095d3cdc102d0e95a84eb7687a3
neskvikspivom <ilya.fedotko09@gmail.com>
2023-01-15 12:22:12 +0300

    добавлен цифровой датчик влажности, последнее время полива

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from classes import Hardware
-import time
+from datetime import datetime
 from guizero import App, PushButton, Text
-tik = 0
-k = 0
-pin = 0
 
-relay = Hardware.Relay(pin, False)
+wlvl_pin = 0
+relay_pin = 0
 
+relay = Hardware.Relay(relay_pin, False)
+wlvl = Hardware.waterLvl(wlvl_pin)
 
 
 
 def water_on():
-    if watering_text.value == 'hello':
+    while wlvl.value == 0:
         relay.on()
-        watering_text.value = 'watering'
-        time.sleep(5)
-        watering_text.value = 'hello'
-
+    else:
+        relay.off()
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        last_watering_time.value == current_time
 
 
 
@@ -27,8 +28,10 @@ app = App(title="Hello world")
 
 watering_text = Text(app, text='hello')
 
-k = k + 1
+last_watering_time = Text(app)
+last_watering_time.repeat(86400000, water_on)
 
 watering_button = PushButton(app, text='start watering', command=water_on)
+
 
 app.display()
--- classes/Hardware.py
+++ classes/Hardware.py
@@ -1,5 +1,9 @@
-from gpiozero import OutputDevice
+from gpiozero import OutputDevice, InputDevice
 
 class Relay(OutputDevice):
     def __init__(self, pin, active_high):
         super(Relay, self).__init__(pin, active_high)
+
+class waterLvl(InputDevice):
+    def __init__(self, pin):
+        super(waterLvl, self).__init__(pin)
